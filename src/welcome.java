69e1bb026250e2d4655e52c022bcf5d550010413
alfi001-tech <usergit033@gmail.com>
2025-11-09 01:43:48 +0530

    hello

--- src/welcome.java
+++ src/welcome.java
@@ -2,7 +2,7 @@ class Solution{
 
     public static void main(String[] args) {
         int a = 100;
-        int b = 15;
+        int b = 155;
         int c = a + b;
         System.out.println(c);
         System.out.println(a);
