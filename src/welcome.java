337b1383c60a7deed6e8a1e61c7a8212555721d8
Alfi <alfi2410work@gmail.com>
2025-11-08 23:49:10 +0530

    changing values

--- src/welcome.java
+++ src/welcome.java
@@ -1,7 +1,7 @@
 class Solution{
 
     public static void main(String[] args) {
-        int a = 10;
+        int a = 100;
         int b = 15;
         int c = a + b;
         System.out.println(c);
