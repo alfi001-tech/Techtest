049cb607bc8cb62f37188712cea17542f14cd871
alfi001-tech <usergit033@gmail.com>
2025-11-08 22:04:59 +0530

    Adding two numbers

--- src/welcome.java
+++ src/welcome.java
@@ -1,6 +1,9 @@
 class Solution{
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Techtest");
+        int a = 10;
+        int b = 15;
+        int c = a + b;
+        System.out.println(c);
     }
 }
